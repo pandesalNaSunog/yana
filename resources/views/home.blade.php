5c3543b178fd08ee2f4d49238bbf33c8dc5b1216
pandesalNaSunog <floresjem8@gmail.com>
2023-09-15 10:33:05 +0800

    get matched feature

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,6 +8,8 @@
         
     </style>
     <title>YANA</title>
+
+    
 </head>
 <body class="vh-100">
     <x-client-nav></x-client-nav>
@@ -22,6 +24,108 @@
             <img src="/yana/home background.jpg" class="img-fluid cover-img" alt="">
         </div>
 
+
+       
+
+    </div>
+
+    <div class="container py-5">
+        <h3 class="fw-bold text-center">OUR FEATURES</h3>
+        <div class="row row-cols-1 row-cols-md-2 row-cols-lg-4 g-3 mt-5">
+            <div class="col">
+                <div class="card shadow feature-card">
+                    <div class="card-body">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="text-primary bi bi-person-fill" viewBox="0 0 16 16">
+                    <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3Zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"/>
+                    </svg>
+                    <p class="fs-4 fw-bold mt-5">YANA Psychologist Specialist</p>
+                    <p class="lead">Offers a unique opportunity for 
+                            freelance psychologists and volunteer 
+                            psychologist to connect with patients 
+                            and offer their services. </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card shadow feature-card">
+                    <div class="card-body">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="text-primary bi bi-hand-thumbs-up-fill" viewBox="0 0 16 16">
+  <path d="M6.956 1.745C7.021.81 7.908.087 8.864.325l.261.066c.463.116.874.456 1.012.965.22.816.533 2.511.062 4.51a9.84 9.84 0 0 1 .443-.051c.713-.065 1.669-.072 2.516.21.518.173.994.681 1.2 1.273.184.532.16 1.162-.234 1.733.058.119.103.242.138.363.077.27.113.567.113.856 0 .289-.036.586-.113.856-.039.135-.09.273-.16.404.169.387.107.819-.003 1.148a3.163 3.163 0 0 1-.488.901c.054.152.076.312.076.465 0 .305-.089.625-.253.912C13.1 15.522 12.437 16 11.5 16H8c-.605 0-1.07-.081-1.466-.218a4.82 4.82 0 0 1-.97-.484l-.048-.03c-.504-.307-.999-.609-2.068-.722C2.682 14.464 2 13.846 2 13V9c0-.85.685-1.432 1.357-1.615.849-.232 1.574-.787 2.132-1.41.56-.627.914-1.28 1.039-1.639.199-.575.356-1.539.428-2.59z"/>
+</svg>
+                    <p class="fs-4 fw-bold mt-5">Free and Subscription-Based 
+                        Options</p>
+                    <p class="lead">Offers free and subscription options for 
+connecting with volunteer or licensed
+ freelance psychologists.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card shadow feature-card">
+                    <div class="card-body">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="text-primary bi bi-clock-fill" viewBox="0 0 16 16">
+  <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71V3.5z"/>
+</svg>
+                    <p class="fs-4 fw-bold mt-5">24/7 Access and Support</p>
+                    <p class="lead">YANA is available 24/7, allowing 
+patients to access the platform and 
+connect with psychologists at 
+their convenience. </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card shadow feature-card">
+                    <div class="card-body">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="text-primary bi bi-chat-fill" viewBox="0 0 16 16">
+  <path d="M8 15c4.418 0 8-3.134 8-7s-3.582-7-8-7-8 3.134-8 7c0 1.76.743 3.37 1.97 4.6-.097 1.016-.417 2.13-.771 2.966-.079.186.074.394.273.362 2.256-.37 3.597-.938 4.18-1.234A9.06 9.06 0 0 0 8 15z"/>
+</svg>
+                    <p class="fs-4 fw-bold mt-5">Connect Anywhere, Anytime</p>
+                    <p class="lead">Effortless Messaging for Mental Health
+Support with YANA: Connect with
+Licensed Freelance or Volunteer 
+Psychologists, Anywhere, Anytime!</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+        <!-- Find Psychologist Section -->
+
+        <div class="row row-cols-1 row-cols-lg-2 g-3 py-5">
+            <div class="col">
+                <p class="fw-bold fs-4 text-primary-color">Find YANA Psychologist</p>
+                <h1 class="fw-bold">Customized Matching with 
+Licensed Psychologists</h1>
+                <p class="lead mt-4">YANA utilizes a sophisticated matching algorithm that takes into account patients' 
+needs,preferences, and goals to connect them with the most suitable licensed 
+freelance psychologists and volunteer psychologist.Patients can choose from a diverse 
+pool of experienced psychologists with various specialties and backgrounds, 
+ensuring personalized and effective care.</p>
+
+                <div>
+                    <span class="me-4"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="text-primary bi bi-check-circle-fill" viewBox="0 0 16 16">
+                    <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
+                    </svg></span><span>100% free app designed to help you find the right Psychologist doctor for you. </span>
+                </div>
+                <div class="mt-2">
+                    <span class="me-4"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="text-primary bi bi-check-circle-fill" viewBox="0 0 16 16">
+                    <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
+                    </svg></span><span>Available Licensed Freelance Psychologist and Volunteer Psychologist.</span>
+                </div>
+                <div class="mt-2">
+                    <span class="me-4"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="text-primary bi bi-check-circle-fill" viewBox="0 0 16 16">
+                    <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
+                    </svg></span><span>Connect with them, Anywhere and Anytime</span>
+                </div>
+                <button class="primary-btn px-5 py-2 mt-3">Get Matched to a Psychologist</button>
+            </div>
+            <div class="col">
+                <img src="/yana/psychologist match 1.jpg" alt="" class="img-fluid">
+                <img style="position: relative; left:50%; bottom: 20%" src="/yana/psychologist match 2.jpg" alt="" class="img-fluid">
+            </div>
+        </div>
     </div>
 </body>
     
