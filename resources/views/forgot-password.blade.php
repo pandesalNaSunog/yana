44e91356c1df4a1a1b8a83dc814a4a710b7f4b91
pandesalNaSunog <floresjem8@gmail.com>
2023-10-04 10:26:25 +0800

    latest

--- resources/views/forgot-password.blade.php
+++ resources/views/forgot-password.blade.php
@@ -8,12 +8,16 @@
 </head>
 <body>
     <div class="container">
-        <div class="mx-auto col-lg-5 card shadow">
-            <div class="card-body text-center">
-                <input type="text" placeholder="XXXXXX" class="form-control">
-                <button class="primary-outline-btn w-100 py-2">Confirm</button>
+        <div class="mx-auto col-lg-5">
+            <h4 class="text-center text-primary-color">Please enter your email below</h4>
+            <div class="card shadow my-5">
+                <div class="card-body text-center">
+                    <input type="text" placeholder="Email" class="form-control">
+                    <button class="primary-outline-btn w-100 py-2">Confirm</button>
+                </div>
             </div>
         </div>
+        
     </div>
 </body>
 </html>
