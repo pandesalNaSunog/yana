311ff242c42a9ad6565f857f5ee5f8c08cb6faba
pandesalNaSunog <floresjem8@gmail.com>
2023-10-07 14:53:15 +0800

    latest

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -10,6 +10,15 @@
 
     <x-admin-nav :active="$active"></x-admin-nav>
     
-
+    <div class="container my-5">
+        <div class="row row-cols-1 row-cols-2">
+            <div class="card shadow" style="border: none">
+                <div class="card-body">
+                    <h4 class="fw-bold">Patients</h4>
+                    <h1 class="fw-bold">200</h1>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
