f2d13846259f525445a36885e5092dbfceef15b9
pandesalNaSunog <floresjem8@gmail.com>
2023-10-07 15:00:41 +0800

    latest

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -6,16 +6,34 @@
     <x-imports></x-imports>
     <title>YANA Administrator | Dashboard</title>
 </head>
-<body>
+<body class="bg-light">
 
     <x-admin-nav :active="$active"></x-admin-nav>
     
     <div class="container my-5">
         <div class="row row-cols-1 row-cols-2">
-            <div class="card shadow" style="border: none">
-                <div class="card-body">
-                    <h4>Patients</h4>
-                    <h1 class="fw-bold fs-1">200</h1>
+            <div class="col">
+                <div class="card shadow" style="border: none">
+                    <div class="card-body">
+                        <h4 class="text-primary-color">Patients</h4>
+                        <h1 class="fw-bold fs-1">200</h1>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card shadow" style="border: none">
+                    <div class="card-body">
+                        <h4 class="text-primary-color">Therapists</h4>
+                        <h1 class="fw-bold fs-1">200</h1>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card shadow" style="border: none">
+                    <div class="card-body">
+                        <h4 class="text-primary-color">Feedbacks</h4>
+                        <h1 class="fw-bold fs-1">200</h1>
+                    </div>
                 </div>
             </div>
         </div>
