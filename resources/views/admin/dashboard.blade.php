2f65443dd99807733dbf6baf57e21b7cb7bc8845
pandesalNaSunog <floresjem8@gmail.com>
2023-10-07 20:04:45 +0800

    latest

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -18,7 +18,7 @@
                         <div class="card shadow" style="border: none">
                             <div class="card-body">
                                 <h4 class="text-primary-color">Patients</h4>
-                                <h1 class="fw-bold fs-1 py-5" style="font-size: 70px!important;">{{$patients}}</h1>
+                                <h1 class="fw-bold fs-1 " style="font-size: 70px!important;">{{$patients}}</h1>
                                 @if($patient_rate_of_increase < 1)
                                 <p class="text-danger">-{{$patient_rate_of_increase . "% for the month of " . date('F')}}</p>
                                 @else
@@ -31,7 +31,7 @@
                         <div class="card shadow" style="border: none">
                             <div class="card-body">
                                 <h4 class="text-primary-color">Therapists</h4>
-                                <h1 class="fw-bold fs-1 py-5" style="font-size: 70px!important;">{{$therapists}}</h1>
+                                <h1 class="fw-bold fs-1 " style="font-size: 70px!important;">{{$therapists}}</h1>
                                 @if($therapist_rate_of_increase < 1)
                                 <p class="text-danger">-{{$therapist_rate_of_increase . "% for the month of " . date('F')}}</p>
                                 @else
@@ -44,7 +44,7 @@
                         <div class="card shadow" style="border: none">
                             <div class="card-body">
                                 <h4 class="text-primary-color">Feedbacks</h4>
-                                <h1 class="fw-bold fs-1 py-5" style="font-size: 70px!important;">{{$feedbacks}}</h1>
+                                <h1 class="fw-bold fs-1 " style="font-size: 70px!important;">{{$feedbacks}}</h1>
                                 @if($feedback_rate_of_increase < 1)
                                 <p class="text-danger">-{{$feedback_rate_of_increase . "% for the month of " . date('F')}}</p>
                                 @else
@@ -57,7 +57,7 @@
                         <div class="card shadow" style="border: none">
                             <div class="card-body">
                                 <h4 class="text-primary-color">Forum Posts</h4>
-                                <h1 class="fw-bold fs-1 py-5" style="font-size: 70px!important;">{{$posts}}</h1>
+                                <h1 class="fw-bold fs-1 " style="font-size: 70px!important;">{{$posts}}</h1>
                                 @if($post_rate < 1)
                                 <p class="text-danger">-{{$post_rate . "% for the month of " . date('F')}}</p>
                                 @else
@@ -70,7 +70,7 @@
                         <div class="card shadow" style="border: none">
                             <div class="card-body">
                                 <h4 class="text-primary-color">Comments Per Post (Avg.)</h4>
-                                <h1 class="fw-bold fs-1 py-5" style="font-size: 70px!important;">{{$average_comments}}</h1>
+                                <h1 class="fw-bold fs-1 " style="font-size: 70px!important;">{{$average_comments}}</h1>
                                 @if($comment_rate < 1)
                                 <p class="text-danger">-{{$comment_rate . "% for the month of " . date('F')}}</p>
                                 @else
@@ -83,7 +83,7 @@
                         <div class="card shadow" style="border: none">
                             <div class="card-body">
                                 <h4 class="text-primary-color">Consultations</h4>
-                                <h1 class="fw-bold fs-1 py-5" style="font-size: 70px!important;">{{$consultations}}</h1>
+                                <h1 class="fw-bold fs-1 " style="font-size: 70px!important;">{{$consultations}}</h1>
                                 @if($consultation_rate < 1)
                                 <p class="text-danger">-{{$consultation_rate . "% for the month of " . date('F')}}</p>
                                 @else
