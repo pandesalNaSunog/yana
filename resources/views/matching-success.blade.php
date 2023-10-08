824b7a7b609eb7c106605c5c18c9d13b27250e2b
pandesalNaSunog <floresjem8@gmail.com>
2023-10-08 16:30:32 +0800

    latest

--- resources/views/matching-success.blade.php
+++ resources/views/matching-success.blade.php
@@ -7,6 +7,16 @@
     <title>Success</title>
 </head>
 <body>
-    {{$tracking_number}}
+
+    <div class="container">
+        <div class="col-lg-5 col text-center">
+            <img src="/envelope.png" style="width: 100px; height: auto" class="img-fluid" alt="">
+            <h4 class="text-primary-color">Your submission has been successfull!</h4>
+            <p class="text-primary-color">We will send you an email for confirmation</p>
+            <h5 class="text-primary-color">Tracking Code: {{$tracking_number}}</h5>
+            <button class="primary-btn mt-3 px-5 py-2">Home</button>
+        </div>
+    </div>
+    
 </body>
 </html>
