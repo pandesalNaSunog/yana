44fadd5ce16c55e3aadce68863c768582cbdcc99
pandesalNaSunog <floresjem8@gmail.com>
2023-10-08 16:35:22 +0800

    latest

--- resources/views/matching-success.blade.php
+++ resources/views/matching-success.blade.php
@@ -12,7 +12,7 @@
         <div class="col-lg-5 col text-center mx-auto">
             <img src="/envelope.png" style="width: 100px; height: auto" class="img-fluid" alt="">
             <h1 class="text-primary-color">Your submission has been successfull!</h1>
-            <h2 class="text-primary-color">We will send you an email for confirmation</h2>
+            <p class="text-primary-color">We will send you an email for confirmation</p>
             <h3 class="text-primary-color">Tracking Code: {{$tracking_number}}</h3>
             <button class="primary-btn mt-3 px-5 py-2">Home</button>
         </div>
